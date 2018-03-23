53c2360b82a000c22a394df79960b39e400351bb
41 <yang41@41s-MacBook-Pro.local>
2018-03-23 15:19:58 +0800

    Revert "COPD ver5.6"
    This reverts commit 5b0ec334da9ec0578988ede1ea79b80110dcbb6b.

--- copd/public/homepage.php
+++ copd/public/homepage.php
@@ -25,21 +25,6 @@ else{
   <!-- Custom fonts for this template-->
   <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <!-- Page level plugin CSS-->
-    
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/popper/popper.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <!-- Page level plugin JavaScript-->
-  <script src="vendor/datatables/jquery.dataTables.js"></script>
-  <!-- <script src="vendor/datatables/dataTables.bootstrap4.js"></script> -->
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin.min.js"></script>
-  <!-- Custom scripts for this page-->
-  <script src="js/sb-admin-datatables.min.js"></script>
-
   <!--   <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">   -->
   <!-- Custom styles for this template-->
   <link href="css/sb-admin.css" rel="stylesheet">
@@ -53,15 +38,29 @@ else{
   <?php require('module.php'); ?>
 
   <!-- content -->
-  <div class="content-wrapper">
-    <div class="container" align="center">
-      <br><h1><strong>COPD walk 善步佳</strong></h1><br>
-      <h4>開發慢性病患智慧健康自我管理系統與裝置 —— 以COPD為例</h4><br>
-      <h4>The development of a smart health self-management system and device for patients with chronic illnesses: Chronic obstruction pulmonary disease</h4><br>
-      <img class="img-fluid" src="pic/main background for management.jpg" height="1440" width="1920" style="border-radius: 5px;">
+  <div class="content-wrapper" style="padding-left: 5px">
+    <div class="container-fluid" align="center" style="margin: auto;">
+	    <!-- /.container-fluid-->
+      <img src="pic/main background for management.jpg" height="720" width="960" style="border-radius: 10px;">
     </div>
+    <!-- /.content-wrapper-->
+
     <!-- Logout Button + Footer -->
     <?php require('footer_and_logout.php'); ?>
+    
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Page level plugin JavaScript-->
+    <script src="vendor/datatables/jquery.dataTables.js"></script>
+    <!-- <script src="vendor/datatables/dataTables.bootstrap4.js"></script> -->
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin.min.js"></script>
+    <!-- Custom scripts for this page-->
+    <script src="js/sb-admin-datatables.min.js"></script>
   </div>
 </body>
 
